c270885b1bc981cbedf0f9e927c2f285ae94d74d
Kye <kyenelson3@gmail.com>
2022-09-24 13:07:57 -0700

    Implemented Astrophotography section of projects

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ function onPress(button)
     document.getElementById(current).style.opacity = 0;
     document.getElementById(current).style.display = "none";
     document.getElementById(button).style.opacity = 100;
-    if (button == "about")
+    if (button == "about" || button == "projects")
     {
         document.getElementById(button).style.display = "grid";
     }
